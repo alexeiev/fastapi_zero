516ab99e7d1172a748423c196384b0a9ffead732
Alexeiev Araujo <ceievfa@gmail.com>
2024-12-26 15:25:40 +0000

    feat: Adicionando nova rota /index

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,26 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from fast_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Ola mundo!'}
+
+
+@app.get('/index', response_class=HTMLResponse)
+def read_index():
+    return """
+    <html>
+      <head>
+        <title>FastAPI do Zero - Alexeiev</title>
+      </head>
+      <body>
+        <h1> olá mundo </h1>
+      </body>
+    </html>"""
--- tests/test_app.py
+++ tests/test_app.py
@@ -7,9 +7,27 @@ from fast_zero.app import app
 
 def test_read_root_deve_retornar_Ola_mundo():
 
-  client = TestClient(app)
+    client = TestClient(app)
 
-  response = client.get('/')
-  assert response.status_code == HTTPStatus.OK
+    response = client.get('/')
+    assert response.status_code == HTTPStatus.OK
 
-  assert response.json() == {'message': 'Ola mundo!'}
+    assert response.json() == {'message': 'Ola mundo!'}
+
+
+def test_read_index_deve_retornar_HTML():
+
+    client = TestClient(app)
+
+    response = client.get('/index')
+    assert response.status_code == HTTPStatus.OK
+
+    assert response.text == """
+    <html>
+      <head>
+        <title>FastAPI do Zero - Alexeiev</title>
+      </head>
+      <body>
+        <h1> olá mundo </h1>
+      </body>
+    </html>"""
