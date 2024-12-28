ab215c0208258449c8fff3e6865ee11653635272
Alexeiev Araujo <ceievfa@gmail.com>
2024-12-28 17:22:26 +0000

    fix: removendo comentários do código

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -100,12 +100,6 @@ def read_user(user_id: int, session=Depends(get_session)):
         raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND, detail='User not found'
         )
-    # if user_id > len(database) or user_id < 1:
-    #     raise HTTPException(
-    #         status_code=HTTPStatus.NOT_FOUND, detail='User not found'
-    #     )
-
-    # return database[user_id - 1]
 
 
 @app.get('/index', response_class=HTMLResponse)
